1161b959d292cd33d762838ff4e748ec076f83b9
unknown <zyx88615@gmail.com>
2019-03-12 00:10:33 -0700

    Fixed pyparagraph counting error

--- PyParagraph/mainpara.py
+++ PyParagraph/mainpara.py
@@ -6,30 +6,30 @@ csvpath = os.path.join(os.path.dirname(__file__),'raw_data', 'paragraph_1.txt')
 csvpath1 = os.path.join(os.path.dirname(__file__),'raw_data', 'paragraph_2.txt')
 
 #read txt file
-with open(csvpath, 'r') as f:
+with open(csvpath,'r') as f:
     data  = f.read()
     p1word = data.split()
     p1word_count = len(data.split())
     p1sentence_count = data.count(".")
-    print("sentences in Paragraph1: "+ str(p1sentence_count))
-    print(f"words in Paragraph1: {p1word_count}")
-    c = {}
+    print("Sentences in Paragraph1: "+ str(p1sentence_count))
+    print(f"Words in Paragraph1: {p1word_count}")
+    totalletter=0
     for word in p1word:
-        c.update({word: len(word)})
-    totalletter= sum(c.values())
-    print(c.items())
+       totalletter = totalletter+len(word)
+    print(f"Letters in Paragraph1: {totalletter}")
 
 with open(csvpath1, 'r') as f:
     # CSV reader specifies delimiter and variable that holds contents
     data1  = f.read()
     p2word = data1.split()
     p2sentence_count = data1.count(".")
-    print("sentences in Paragraph2: " + str(p2sentence_count-1))
+    print("Sentences in Paragraph2: " + str(p2sentence_count-1))
     p2word_count = len(data1.split())
-    print(f"words in Paragraph2: {p2word_count}")
-    d={word:len(word) for word in p2word}
-    print(d.items())
-    totalletter1=sum(d.values())
+    print(f"Words in Paragraph2: {p2word_count}")
+    totalletter1=0
+    for word in p2word:
+        totalletter1=totalletter1+len(word)
+    print(f"Letters in Paragraph2: {totalletter1}")
     print("\n")
 
 
